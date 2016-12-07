e03c5081555aae8f00f46cd84ff424be5572a93e
Pancheri Renato (ACC0083848) <renato.pancheri@sci-b106pc051x.unitn.it>
2016-12-07 10:17:27 +0100

    aggiunti commenti

--- create_results.js
+++ create_results.js
@@ -1,3 +1,8 @@
+/*
+  funzione principale che viene chiamata dal server, prende in ingresso gli attributi della richiesta 
+  e restituisce un oggetto valido per il binding del template
+
+*/
 function main(request){
 	console.log(request);
 	return {results:[{fonte:"asd",link:"/",shortlink:"home"}]};
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var bind = require('bind');
 var ip='127.0.0.1';
 var port=1337;
 
-var headers = {};
+var headers = {};//definisco gli header di default
 headers["Access-Control-Allow-Origin"] = "*"; //for cross enviroment request
 headers["Access-Control-Allow-Methods"] = "GET";//methods allowed to responce
 headers["Access-Control-Allow-Credentials"] = false;
@@ -24,16 +24,16 @@ var results=require('./create_results.js');
 var app = express();
 
 app.set('port', (process.env.PORT || port));
-app.use(express.static('client_files'));
+app.use(express.static('client_files'));//mappa tutte le richieste di files del tipo http://ip:port/* in client_files/*
 //use: for both POST and GET
 
-app.get('/search_results.html',function(request,response){
+app.get('/search_results.html',function(request,response){//search_results.html è il template
 	response.writeHead(200, headers);
 	var url_parts = url.parse(request.url, true);
-	var getVar = url_parts.query; //aggancio un nuovo attributo
-	var ret=results.main(getVar);
+	var getVar = url_parts.query; //estraggo gli attributi dalla richiesta
+	var ret=results.main(getVar);//chiamo la funzione main del modulo results che restituisce tutti i parametri per il binding del template
 	bind.toFile('tpl/search_results.tpl',
-	ret,
+	ret,//risultati dalla funzione results.main()
     function(data) 
     {
         //write response
