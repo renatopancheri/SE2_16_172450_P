cc4ddbed5c6226afba64afbc79a9879cb8e1347c
renatopancheri <renato.pancheri@gmail.com>
2016-12-11 05:50:08 -0800

    nuova serie di test

--- spec/filespec.js
+++ spec/filespec.js
@@ -1,9 +1,14 @@
 var request=require("request");
 var url="http://127.0.0.1:1337";
 describe("testGet", function() {
-	it("/", testGet("/"));
-	it("/index.html", testGet("/index.html"));
-	it("/search_results.html", testGet("/search_results.html"));
+	it("/", testGet("/",200));
+	it("/index.html", testGet("/index.html",200));
+	it("/percorsoinesistente", testGet("/percorsoinesistente",404));
+	it("/search_results.html", testGet("/search_results.html",400));
+	it("/search_results.html?robe=123", testGet("/search_results.html?robe=123",400));
+	it("/search_results.html?search=as&abc=123", testGet("/search_results.html?search=as&abc=123",400));
+	it("/search_results.html?search=", testGet("/search_results.html?search=",400));
+	it("/search_results.html?search=123", testGet("/search_results.html?search=123",200));
 });
 
 
@@ -12,13 +17,13 @@ describe("testGet", function() {
 
 
 */
-function testGet(percorso){
+function testGet(percorso,statusCode){
 	var ret = function(done){
 		expect(typeof percorso).toBe("string");//controllo che sia stata data una stringa in ingresso
 		request.get(
 			url+percorso,//fattio il get so url_del_server+percorso_specificato
 			function (error,response,body){
-				expect(response.statusCode).toBe(200);//200 OK
+				expect(response.statusCode).toBe(statusCode);//200 OK
 				done();//callback perché get è asincrono
 			});
 	}
